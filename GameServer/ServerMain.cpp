e0082c467ae69c157396dc3eeb85f4d7b148f4b7
Wallace Oliveira <Wallace9655@gmail.com>
2021-02-14 23:50:55 +0000

    Added static tickrate and improved server code readability

--- GameServer/ServerMain.cpp
+++ GameServer/ServerMain.cpp
@@ -13,7 +13,7 @@ struct SharedInfo
 	float *last_response;
 };
 
-
+//Wait for threads to finish, release memory and resources.
 void ShutdownServer(SharedInfo* info)
 {
 	info->is_running = 0;
@@ -34,6 +34,7 @@ void ShutdownServer(SharedInfo* info)
 	WSACleanup();
 }
 
+//Init server and allocate memory
 int InitServer(SharedInfo * info)
 {
 	*info = {};
@@ -82,57 +83,127 @@ int InitServer(SharedInfo * info)
 	return 0;
 }
 
+//Calculate time since last tick
+float time_since(const LARGE_INTEGER& t, const LARGE_INTEGER& frequency)
+{
+	LARGE_INTEGER now;
+	QueryPerformanceCounter(&now);
+
+	return float( now.QuadPart - t.QuadPart) / float(frequency.QuadPart);
+}
+
+void waitForTick(const bool& sleep_granularity_was_set, const LARGE_INTEGER& tick_start,const LARGE_INTEGER& frequency)
+{
+	float time_taken_s = time_since(tick_start, frequency);
+	
+	while(time_taken_s < ServerSettings::TICK_RATE)
+	{
+		if(sleep_granularity_was_set)
+		{
+			DWORD time_to_wait_ms = DWORD((ServerSettings::TICK_RATE - time_taken_s)* 1000);
+			if(time_to_wait_ms > 0)
+			{
+				Sleep(time_to_wait_ms);
+			}
+		}
+
+		time_taken_s = time_since(tick_start, frequency);
+	}
+}
+
+void HandleClientInput(const char & client_input, SharedInfo * const info)
+{
+	switch (client_input)
+		{
+			//TODO: Replace codes by enumerators instead
+			case 'w':
+				info->player_state[0].y++;
+				break;
+
+			case 'a':
+				info->player_state[0].x--;
+				break;
+
+			case 's':
+				info->player_state[0].y--;
+				break;
+
+			case 'd':
+				info->player_state[0].x++;
+				break;
+
+			case 'q':
+				info->is_running = 0;
+				break;
+
+			default:
+				printf("unhandled input: %c \n", client_input);
+		}
+}
+
 void CommunicationHandler(void* info_ptr)
 {
-	SharedInfo info = *(SharedInfo*)info_ptr;
+	SharedInfo * info = (SharedInfo*)info_ptr;
 	char buffer[SOCKET_BUFFER_SIZE];
 
-	while (info.is_running)
+	//TODO: Init timer here
+	UINT sleep_glanularity_ms = 1;
+	bool sleep_glanularity_set = timeBeginPeriod(sleep_glanularity_ms) == TIMERR_NOERROR;
+	
+	LARGE_INTEGER clock_frequency;
+	QueryPerformanceFrequency(&clock_frequency);
+
+	while (info->is_running)
 	{
-		int bytes_received = 0;
+		//Obtain start time of tick, to synchronize tick rate at the end of the cycle
+		LARGE_INTEGER tick_start_time;
+		QueryPerformanceCounter(&tick_start_time);
+
+		//Receive message from client
 		int flags = 0;
 		SOCKADDR_IN from;
 		int from_size = sizeof(from);
 
-		bytes_received = recvfrom(info.sock, buffer, SOCKET_BUFFER_SIZE, flags, (SOCKADDR*)&from, &from_size);
+		int bytes_received = recvfrom(info->sock, buffer, SOCKET_BUFFER_SIZE, flags, (SOCKADDR*)&from, &from_size);
 		if (bytes_received == SOCKET_ERROR)
 		{
 			printf("recvfrom return SOCKET_ERROR, WSAGetLastError() %d", WSAGetLastError());
 			break;
 		}
 
-		char client_input = buffer[0];
-		printf("%d.%d.%d.%d:%d - %s", from.sin_addr.S_un.S_un_b.s_b1, from.sin_addr.S_un.S_un_b.s_b2,
+		printf("%d.%d.%d.%d:%d - %s\n", from.sin_addr.S_un.S_un_b.s_b1, from.sin_addr.S_un.S_un_b.s_b2,
 			from.sin_addr.S_un.S_un_b.s_b3, from.sin_addr.S_un.S_un_b.s_b4, ntohs(from.sin_port), buffer);
-	
-		switch (client_input)
+
+		char client_input = buffer[0];
+		HandleClientInput(client_input, info);
+
+		//Prepare client reply
+		int write_index = 0;	//Also used as the buffer length
+
+		memcpy(&buffer[write_index], &info->player_state[0].x, sizeof(info->player_state[0].x));
+		write_index += sizeof(info->player_state[0].x);
+
+		memcpy(&buffer[write_index], &info->player_state[0].y, sizeof(info->player_state[0].y));
+		write_index += sizeof(info->player_state[0].y);
+
+		memcpy(&buffer[write_index], &info->is_running, sizeof(info->is_running));
+		write_index += sizeof(info->is_running);
+
+		//Reply to client
+		flags = 0;
+		SOCKADDR* to = (SOCKADDR*) &from;
+		int to_length = sizeof(from);
+
+		if(sendto(info->sock,buffer, write_index, flags, to, to_length) == SOCKET_ERROR)
 		{
-			case 'w':
-				info.player_state[0].y++;
-				break;
-
-			case 'a':
-				info.player_state[0].x--;
-				break;
-
-			case 's':
-				info.player_state[0].y--;
-				break;
-
-			case 'd':
-				info.player_state[0].x++;
-				break;
-
-			case 'q':
-				info.is_running = 0;
-				break;
-
-			default:
-				printf("unhandled input: %c \n", client_input);
+			printf("failed to send packet: %d", WSAGetLastError());
+			break;
 		}
-
-
+		
+		waitForTick(sleep_glanularity_set, tick_start_time, clock_frequency);
 	}
+
+
 }
 
 int main(int argc, char** argv)
@@ -154,7 +225,7 @@ int main(int argc, char** argv)
 	CommunicationHandler(&info);
 
 	//TODO: Server Console  ( maybe add to a separate thread )
-	//TODO: Add thread wait
+	//TODO: Add thread join
 
 
 	ShutdownServer(&info);
