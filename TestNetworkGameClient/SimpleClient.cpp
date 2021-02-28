5f76c564be61a1a123b4c1ae9fe95f0a6d8fd0d1
Wallace Oliveira <Wallace9655@gmail.com>
2021-02-28 05:10:03 +0000

    Improved test client; Fixed bugs and added a few features

--- TestNetworkGameClient/SimpleClient.cpp
+++ TestNetworkGameClient/SimpleClient.cpp
@@ -1,5 +1,6 @@
 #include "../GameServer/NetworkHandler.h"
 #include <Ws2tcpip.h>
+#include <conio.h>
 
 int main()
 {
@@ -10,21 +11,56 @@ int main()
 		return -1;
 	}
 
-	SOCKADDR_IN server_address;
-	server_address.sin_family = AF_INET;
-	server_address.sin_port = htons(9999);
-	inet_pton(AF_INET,"127.0.0.1", &server_address.sin_addr);
-
-	char message[SOCKET_BUFFER_SIZE];
-	gets_s(message, SOCKET_BUFFER_SIZE);
-
-	int flags = 0;
-	if (sendto(sock, message, SOCKET_BUFFER_SIZE, flags, (SOCKADDR *) &server_address, sizeof(server_address)) ==
-		SOCKET_ERROR)
+	while (true)
 	{
-		printf("sendto failed: %d", WSAGetLastError());
-		return -1;
+		SOCKADDR_IN server_address;
+		server_address.sin_family = AF_INET;
+		server_address.sin_port = htons(9999);
+		inet_pton(AF_INET, "127.0.0.1", &server_address.sin_addr);
+
+		char message[SOCKET_BUFFER_SIZE];
+		message[0] = _getch();
+
+		int flags = 0;
+		if (sendto(sock, message, SOCKET_BUFFER_SIZE, flags, (SOCKADDR*)&server_address, sizeof(server_address)) ==
+			SOCKET_ERROR)
+		{
+			printf("sendto failed: %d", WSAGetLastError());
+			return -1;
+		}
+
+		SOCKADDR* from = (SOCKADDR *) &server_address;
+		int from_size = sizeof(*from);
+
+		int bytes_received = recvfrom(sock, message, SOCKET_BUFFER_SIZE, flags, from, &from_size);
+		if (bytes_received == SOCKET_ERROR)
+		{
+			printf("recvfrom return SOCKET_ERROR, WSAGetLastError() %d", WSAGetLastError());
+			break;
+		}
+
+		//Prepare client reply
+		int write_index = 0;	//Also used as the buffer length
+		int x, y, run;
+		x = y = run = 0;
+
+		memcpy(&x, &message[write_index], sizeof(int));
+		write_index += sizeof(int);
+
+		memcpy(&y, &message[write_index], sizeof(int));
+		write_index += sizeof(int);
+
+		memcpy(&run, &message[write_index], sizeof(int));
+
+		printf("Received from server: X:%d, Y:%d, is_running: %d\n", x, y, run);
+		if (!run)
+		{
+			break;
+		}
 	}
+
+	closesocket(sock);
+	WSACleanup();
 
 	return 0;
 }
